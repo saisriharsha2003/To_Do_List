0b4ca1ab14549a1a7ea2377d81f2395e57bc3a1d
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2025-06-15 20:42:45 +0530

    updated gitignore

--- todovibe-backend/config.js
+++
@@ -1,3 +0,0 @@
-export const MONGODB_URL = "mongodb+srv://arjunsai2035:ArjunSai%402035@cluster0.0ve0k.mongodb.net/todovibe"
-export const JWT_SECRET = "saisri2005"
-export const EMAIL_USER = "codegenie.aitutor@gmail.com"
